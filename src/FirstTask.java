ad659a9aa5ace817dc449bc92341909357aa3885
Viacheslav Hrytskevych <hrytskevych.v@gmail.com>
2023-07-15 15:02:39 +0300

    the first is solved

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,5 +1,18 @@
 public class FirstTask {
 
-}
+    static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
 
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
 
